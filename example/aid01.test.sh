5213a67b588612f7435e23170f29f280cb50ff97
leon123858 <a0970785699@gmail.com>
2024-05-30 09:18:56 +0000

    new aid

--- example/aid01.test.sh
+++ example/aid01.test.sh
@@ -10,22 +10,7 @@ contract_address=$(deploycontract)
 echo "$contract_address"
 bitcoin-cli generate 1
 bitcoin-cli callcontract "$contract_address" "registerNewUser" "user1" "password1"
+bitcoin-cli callcontract "$contract_address" "registerNewUser" "user2" "password2"
 bitcoin-cli generate 1
-bitcoin-cli dumpcontractmessage "$contract_address" "login" "user1" "password1" > log.txt
-user_address=$(grep "aid" log.txt | awk -F'"' '{print $4}')
-echo "$user_address"
-rm log.txt
-bitcoin-cli callcontract "$contract_address" "setCoin" "$user_address" "coin1"
-bitcoin-cli callcontract "$contract_address" "setCoin" "$user_address" "coin2"
-bitcoin-cli callcontract "$contract_address" "setCoin" "$user_address" "coin3"
-bitcoin-cli callcontract "$contract_address" "setCoin" "$user_address" "coin4"
-bitcoin-cli callcontract "$contract_address" "setCoin" "$user_address" "coin5"
-bitcoin-cli callcontract "$contract_address" "setCoin" "$user_address" "coin5"
-bitcoin-cli generate 1
-bitcoin-cli dumpcontractmessage "$contract_address" "getCoins" "$user_address"
-bitcoin-cli callcontract "$contract_address" "removeCoin" "$user_address" "coin5"
-bitcoin-cli callcontract "$contract_address" "removeCoin" "$user_address" "coin4"
-bitcoin-cli callcontract "$contract_address" "removeCoin" "$user_address" "coin3"
-bitcoin-cli dumpcontractmessage "$contract_address" "getCoins" "$user_address"
-bitcoin-cli generate 1
-bitcoin-cli dumpcontractmessage "$contract_address" "getCoins" "$user_address"
+bitcoin-cli dumpcontractmessage "$contract_address" "get"
+bitcoin-cli dumpcontractmessage "$contract_address" "readSign" "user1"
